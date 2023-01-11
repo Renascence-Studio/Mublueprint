6d8d5365f904377faea156cc8a42c6dbb043974a
chemiofitor <783626185@qq.com>
2023-01-11 15:10:48 +0800

    setup

---
+++ src/main/java/hlft/fabric/mublueprint/Mublueprint.java
@@ -0,0 +1,10 @@
+package hlft.fabric.mublueprint;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mublueprint implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
