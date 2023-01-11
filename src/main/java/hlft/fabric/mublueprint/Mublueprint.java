694b13b99a85ed644f173f10d578a1f862bc5e62
chemiofitor <783626185@qq.com>
2023-01-12 01:44:24 +0800

    1.0-SNAPSHOT push

--- src/main/java/hlft/fabric/mublueprint/Mublueprint.java
+++ src/main/java/hlft/fabric/mublueprint/Mublueprint.java
@@ -1,10 +1,55 @@
 package hlft.fabric.mublueprint;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static hlft.fabric.mublueprint.MublueprintTab.MAIN;
 
 public class Mublueprint implements ModInitializer {
+    public static final String MOD_ID = "mublueprint";
+
+    public static final List<Item> ITEMS = new ArrayList<>();
+
     @Override
     public void onInitialize() {
+        createItem("blueprint", commonItem());
+        createItem("broad_axe_head_blueprint", commonItem());
+        createItem("broad_blade_blueprint", commonItem());
+        createItem("large_plate_blueprint", commonItem());
+        createItem("pick_head_blueprint", commonItem());
+        createItem("round_plate_blueprint", commonItem());
+        createItem("small_axe_head_blueprint", commonItem());
+        createItem("small_blade_blueprint", commonItem());
+        createItem("tool_binding_blueprint", commonItem());
+        createItem("tool_handle_blueprint", commonItem());
+    }
 
+    public static Identifier asId(String path) {
+        return new Identifier(MOD_ID, path);
+    }
+
+    public static ItemStack getIconItem() {
+        return Registry.ITEM.get(asId("blueprint")).getDefaultStack();
+    }
+
+    public static FabricItemSettings itemPro() {
+        return new FabricItemSettings().group(MAIN);
+    }
+
+    public static Item commonItem() {
+        return new Item(itemPro().maxCount(1).rarity(Rarity.RARE));
+    }
+
+    public static void createItem(String id, Item item) {
+        Registry.register(Registry.ITEM, asId(id), item);
+        ITEMS.add(item);
     }
 }
---
+++ src/main/java/hlft/fabric/mublueprint/MublueprintTab.java
@@ -0,0 +1,16 @@
+package hlft.fabric.mublueprint;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+import static hlft.fabric.mublueprint.Mublueprint.ITEMS;
+import static hlft.fabric.mublueprint.Mublueprint.asId;
+
+public class MublueprintTab {
+    public static final ItemGroup MAIN = FabricItemGroupBuilder
+            .create(asId("main"))
+            .icon(Mublueprint::getIconItem)
+            .appendItems(itemStacks -> itemStacks.addAll(ITEMS.stream().map(ItemStack::new).toList()))
+            .build();
+}
